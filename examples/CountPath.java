e9c9ff291ae664e0b5c24efcd22e7dc154d60605
Gaurav Gupta <gauravava2003@yahoo.com>
2019-06-18 23:31:08 -0500

    fixed the issues with countpath

--- examples/CountPath.java
+++ examples/CountPath.java
@@ -9,21 +9,21 @@ class CountPath{
 	}
 
 	public void countPath(int noOfRows, int noOfColumns){
-		int pathCount = countPath(0, 0, 0, noOfRows, noOfColumns);
+		int pathCount = countPath(0, 0, noOfRows, noOfColumns);
 		System.out.println("count path for rows: " + noOfRows + " and columns: " + noOfColumns+ " is: " + pathCount);
 	}
 
-	private int countPath(int pathCount, int rowLoc, int colLoc, int noOfRows, int noOfColumns){
-		if(rowLoc == noOfRows-1 && colLoc == noOfColumns -1)
+	private int countPath(int rowLoc, int colLoc, int noOfRows, int noOfColumns){
+		if(rowLoc == noOfRows && colLoc == noOfColumns)
 			return 1;
 
-		if(rowLoc == noOfRows || colLoc == noOfColumns)
+		if(rowLoc == noOfRows+1 || colLoc == noOfColumns+1)
 			return 0;
 		
-		pathCount = pathCount + countPath(pathCount, rowLoc+1, colLoc, noOfRows, noOfColumns);
+		int downPaths = countPath(rowLoc+1, colLoc, noOfRows, noOfColumns);
 
-		pathCount = pathCount + countPath(pathCount, rowLoc, colLoc+1, noOfRows, noOfColumns);
+		int rightPaths = countPath(rowLoc, colLoc+1, noOfRows, noOfColumns);
 
-		return pathCount;
+		return downPaths+rightPaths;
 	}
 }
